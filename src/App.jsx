3aa78b7afb8b87afa9faa685f7087f0a390e1ce4
aymanqabbal <aymanqabbal.arts@gmail.com>
2023-02-02 21:04:11 +0100

    added navbar and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Home from "./pages/Home";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Contact from "./pages/Contact";
+import { Routes, Route } from "react-router";
 function App() {
   return (
     <div className="bg-black text-white font-unbounded w-screen h-screen  py-0 my-0 overflow-x-hidden">
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div className="contact w-screen h-screen bg-black">
+      <h1>Contact</h1>
+    </div>
+  );
+}
